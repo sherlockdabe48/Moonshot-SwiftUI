05c6f6df0a5674b87ae06eb4d399d162f435802e
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-07-06 22:39:55 +0700

    Update UI

--- Moonshot2/ContentView.swift
+++ Moonshot2/ContentView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    @State private var showingCrews = false
+    var showCrewsButtonTitle: String {
+        return showingCrews ? "Show Launch Date" : "Show Crews"
+    }
     var body: some View {
         
         NavigationView {
@@ -24,11 +28,27 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text(mission.displayName)
                             .font(.headline)
-                        Text(mission.formattedLaunchDate)
+                        if !showingCrews {
+                            Text(mission.formattedLaunchDate)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        } else {
+                            HStack {
+                                ForEach(mission.crew, id: \.name) { member in
+                                    Text(member.name)
+                                        .font(.subheadline)
+                                        .textCase(.uppercase)
+                                        .foregroundColor(.secondary)
+                                }
+                            }
+                        }
                     }
                 }
             }
             .navigationBarTitle("Moonshot")
+            .navigationBarItems(trailing : Button("\(showCrewsButtonTitle)"){
+                self.showingCrews.toggle()
+            })
         }
     }
 }
